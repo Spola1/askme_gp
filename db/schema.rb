d816b3ae0c7ee02db471ce119d0c0d4a13b69d38
Spola1 <1111.22224@mail.ru>
2022-04-25 12:45:13 +0300

    small-fixes

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2022_04_24_140324) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "hidden", default: false
+    t.boolean "hidden", default: false, null: false
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
