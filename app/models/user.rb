0c46dc5a174af3fdeea175f338f1878f85c877ff
Spola1 <1111.22224@mail.ru>
2022-05-01 13:21:41 +0300

    regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
 
+  REGEX_NICKNAME = /\A[0-9a-z_]+\z/
+  REGEX_COLOR = /\A#\h{3}{1,2}\z/
+
   before_save :downcase_nickname
 
   validates :email, presence: true, uniqueness: true,
     format: { with: URI::MailTo::EMAIL_REGEXP }
 
   validates :nickname, uniqueness: true, presence: true,
-    length: { maximum: 40 }, format: { with: /\A[0-9a-z_]+\z/ }
+    length: { maximum: 40 }, format: { with: REGEX_NICKNAME }
 
-  validates :color, format: { with: /\A#(?:\h{3}){1,2}\z/ }
+  validates :color, format: { with: REGEX_COLOR }
 
   def downcase_nickname
     nickname.downcase!
