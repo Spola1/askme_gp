204f182b5454c3f0c6856143089ab484c338188c
Spola1 <1111.22224@mail.ru>
2022-05-03 16:28:21 +0300

    add question author

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   validates :body, presence: true, length: { maximum: 280 }
 end
