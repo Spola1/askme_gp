53b825624fdbf08ca6fdd3a4a03200c5eb34a3e3
Spola1 <1111.22224@mail.ru>
2022-05-21 14:35:34 +0300

    dependet:destroy + fixes views

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,17 +1,10 @@
 class Hashtag < ApplicationRecord
   extend FriendlyId
 
-  has_many :hashtag_links
+  has_many :hashtag_links, dependent: :destroy
   has_many :questions, through: :hashtag_links
 
   validates :text, presence: true
-
-  scope :with_questions, lambda {
-                           left_joins(:hashtag_links)
-                             .where.not(hashtag_links: { question_id: nil })
-                             .distinct
-                             .order(created_at: :desc)
-                         }
 
   friendly_id :text, use: :slugged
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
-  has_many :hashtag_links
+  has_many :hashtag_links, dependent: :destroy
   has_many :hashtags, through: :hashtag_links
 
   validates :body, presence: true, length: { maximum: 280 }
